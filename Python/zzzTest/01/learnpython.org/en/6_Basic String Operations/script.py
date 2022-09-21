a34e74ed4356bd9c1ef742de515c6aace1860ade
lister <list_lfa@ymail.com>
2022-09-21 19:49:57 +0800

    6

---
+++ Python/zzzTest/01/learnpython.org/en/6_Basic String Operations/script.py
@@ -0,0 +1,26 @@
+a="Hello World!"
+print(len(a))
+
+print(a.index("o"))
+
+print(a.count("l"))
+
+print(a[3:7])
+
+print(a[3:7:2])
+
+print(a[3:7:1])
+
+print(a[::-1])
+
+print(a.upper())
+print(a.lower())
+
+print(a.startswith("Hello"))
+print(a.startswith("hello"))
+print(a.endswith("adsasdasdf"))
+
+b=a.split(" ")
+print(b)
+
+
