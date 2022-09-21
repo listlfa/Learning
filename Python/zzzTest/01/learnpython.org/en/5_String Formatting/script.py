9d1dd99361827ef9c5bfa11e8974d0c2c6b9b86b
lister <list_lfa@ymail.com>
2022-09-21 19:19:47 +0800

    5

---
+++ Python/zzzTest/01/learnpython.org/en/5_String Formatting/script.py
@@ -0,0 +1,13 @@
+name="john"
+print("name is %s" % name)
+
+age = 10
+print("%s is %d years old" % (name, 10))
+
+a=[1,2,3]
+print("list %s" % a)
+#print("list %d" % a)   #print("list %s" % a)
+
+data=["john", "doe",52.43]
+format="Hello"
+print(format+ " "+data[0]+" "+data[1]+". you balance is $%.2f" %data[2])
