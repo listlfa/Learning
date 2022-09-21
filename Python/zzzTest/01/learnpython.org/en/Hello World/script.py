d4a185f7968a091cb6e0e24520ea375222251b56
lister <list_lfa@ymail.com>
2022-09-21 13:11:50 +0800

    2

--- Python/zzzTest/01/learnpython.org/en/Hello World/script.py
+++ Python/zzzTest/01/learnpython.org/en/Hello World/script.py
@@ -1,1 +1,4 @@
-print("this line will be printed")
+x=1
+if x==1:
+    print("this line will be printed")
+print("goodbye world")
---
+++ Python/zzzTest/01/learnpython.org/en/Variables and Types/script.py
@@ -0,0 +1,32 @@
+x=7
+y=7.0
+z=float(7)
+print(x)
+print(y)
+print(z)
+print(x+y)
+print(x+y+z)
+
+a='hi'
+b="hi"
+print(a)
+print(b)
+print(a+b)
+
+c="i'm"
+d="i''m"
+e="i''''m"
+print(c)
+print(d)
+print(e)
+print(c+d+e)
+
+f,g=1,2
+print(f);
+print(f,g)
+
+if isinstance(1,int):
+    print("int : %d" % f)
+    print("float : %f" % f)
+print("string %s" % c)
+print("string %s" % f)
